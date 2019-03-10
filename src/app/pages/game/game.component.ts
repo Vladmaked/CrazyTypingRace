3b95a34f77680c41e3ffb6262768e326595fa141
YulyKo <juliakotenko.forstudy@gmail.com>
2019-03-10 23:47:08 +0200

    writing and checking of input text were done

--- src/app/pages/game/game.component.ts
+++ src/app/pages/game/game.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.css']
 })
+
 export class GameComponent implements OnInit {
+  text = 'my text';
+  indexLetter = 0;
+  indexSpan = 0;
+  letters = [{
+    symbol: this.text.split(''),
+    display: false,
+    correct: false
+  }];
+  typedLetter: string;
+
+  @HostListener('document:keypress', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) {
+    this.typedLetter = event.key;
+    this.checkLetter();
+  }
 
   constructor() { }
 
   ngOnInit() {
+    for (let i = 0; i < this.letters[0].symbol.length; i++) {
+      const child = document.createElement('span');
+      child.innerText = this.letters[0].symbol[i];
+      document.getElementById('textZone').appendChild(child);
+      child.setAttribute('id', String(i));
+      child.setAttribute('class', 'text');
+    }
   }
 
+  checkLetter() {
+    const span = document.getElementById(String(this.indexSpan));
+    if ( this.letters[0].symbol[this.indexLetter] === this.typedLetter ) {
+      this.letters[0].correct = true;
+      this.indexLetter ++;
+      this.indexSpan ++;
+      span.style.backgroundColor = '#67ff65';
+    } else {
+      span.style.backgroundColor = 'coral';
+    }
+  }
 }
