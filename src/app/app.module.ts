d16547dca392642921f18d855397c97c93609f81
YulyKo <juliakotenko.forstudy@gmail.com>
2019-03-10 17:02:47 +0200

    created game page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GameComponent } from './pages/game/game.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule
