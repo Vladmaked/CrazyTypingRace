194c576ace24b6dd7234dc31587341e4ca2dde88
YulyKo <juliakotenko.forstudy@gmail.com>
2019-03-12 14:17:31 +0200

    created simple admin page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,21 +5,29 @@ import { AppComponent } from './app.component';
 import { GameComponent } from './pages/game/game.component';
 import { RouterModule, Routes } from '@angular/router';
 import {HttpClientModule} from '@angular/common/http';
-import {ServiceService} from "./service.service";
+import {ServiceService} from './service.service';
+import { AdminComponent } from './pages/admin/admin.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 const routes: Routes = [
-  { path: '**', component: GameComponent }
+  { path: 'game', component: GameComponent },
+  { path: 'admin', component: AdminComponent},
+  { path: '**', component: AppComponent }
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    GameComponent
+    GameComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(routes),
-    HttpClientModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [ServiceService],
   bootstrap: [AppComponent]
---
+++ src/app/pages/admin/admin.component.ts
@@ -0,0 +1,39 @@
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup} from '@angular/forms';
+import { ServiceService } from '../../service.service';
+
+@Component({
+  selector: 'app-admin',
+  templateUrl: './admin.component.html',
+  styleUrls: ['./admin.component.css']
+})
+
+export class AdminComponent implements OnInit {
+  createNewTextForm: FormGroup;
+
+  constructor( private formBuilder: FormBuilder,
+               private configServers: ServiceService ) {
+    this.createNewTextForm = this.createFormGroup();
+  }
+
+  createFormGroup() {
+    return new FormGroup({
+      category: new FormControl(),
+      theme: new FormControl(),
+      body: new FormControl()
+    });
+  }
+
+  ngOnInit() {
+  }
+
+  onSubmit(  ) {
+    this.configServers.putText(this.createNewTextForm.value)
+      .subscribe(
+        (response) => console.log(response),
+        (error) => console.log(error)
+      );
+  }
+
+
+}
