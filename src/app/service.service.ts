ad381e9f068f081a8393f558e2d9c62f918d68c2
YulyKo <juliakotenko.forstudy@gmail.com>
2019-03-28 15:54:56 +0200

    Revert "putText -> postText"
    This reverts commit 5ce48beda2f9f47696243cc45e45693a582ba648.

--- src/app/service.service.ts
+++ src/app/service.service.ts
@@ -9,7 +9,7 @@ export class ServiceService {
   private api = environment;
   constructor( private http: HttpClient ) { }
 
-  postText(servers: object) {
+  putText( servers: object) {
     alert('The results are:' + servers);
     return this.http.post( this.api.apiUrl + '/db/add/text', JSON.stringify(servers), {
       headers: new HttpHeaders({
