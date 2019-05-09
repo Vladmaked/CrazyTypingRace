ffce681bb7faee3a52912ff2535d60b11dd1a192
YulyKo <juliakotenko.forstudy@gmail.com>
2019-05-09 14:40:41 +0300

    some fix in animation

--- src/app/pages/game/game.component.ts
+++ src/app/pages/game/game.component.ts
@@ -1,6 +1,5 @@
-import {Component, HostListener, Injectable, OnInit} from '@angular/core';
+import {Component, ElementRef, HostListener, Injectable, OnInit, Renderer2, ViewChild} from '@angular/core';
 import {HtmlService} from '../../html.service';
-import {FirstPageComponent} from '../first-page/first-page.component';
 
 @Component({
   selector: 'app-game',
@@ -13,6 +12,24 @@ import {FirstPageComponent} from '../first-page/first-page.component';
 })
 
 export class GameComponent implements OnInit {
+  @ViewChild('mountains0')
+  mountains0: SVGElement;
+
+  @ViewChild('mountains1')
+  mountains1: ElementRef;
+
+  @ViewChild('treeContainer0')
+  treeContainer0: ElementRef;
+
+  @ViewChild('treeContainer1')
+  treeContainer1: ElementRef;
+
+  @ViewChild('grassContainer0')
+  grassContainer0: ElementRef;
+
+  @ViewChild('grassContainer1')
+  grassContainer1: ElementRef;
+
   text = this.htmlService.textOnService;
   indexLetter = 0;
   indexSpan = 0;
@@ -24,47 +41,13 @@ export class GameComponent implements OnInit {
   typedLetter: string;
   start;
   finish;
-  timeOfThisLatter;
+  timeOfThisLatter: number;
   timeOfLastLatter: number;
   speedCar = 7;
   speedOpponent = 5;
   positionCar = 0;
   positionOpponent = 0;
-  backgroundObjects = [
-    {
-      id: document.getElementById('mountains0'),
-      position: 0,
-      distance: 100
-    },
-    {
-      id: document.getElementById('mountains1'),
-      position: 0,
-      distance: 100
-    },
-    {
-      id: document.getElementById('treeContainer0'),
-      position: 0,
-      distance: 3
-    },
-    {
-      id: document.getElementById('treeContainer1'),
-      position: 0,
-      distance: 3
-    },
-    {
-      id: document.getElementById('grassContainer0'),
-      position: 0,
-      distance: 1
-    },
-    {
-      id: document.getElementById('grassContainer1'),
-      position: 0,
-      distance: 1
-    }
-  ];
-  positionMountains = 0;
-  arrayOfPositionByTree = [0, 0, 0, 0, 0, 0, 0];
-  arrayOfPositionByGrass = [];
+  backgroundObjects;
   arrayOfGrassIds = [];
 
   @HostListener('document:keypress', ['$event'])
@@ -73,7 +56,8 @@ export class GameComponent implements OnInit {
     this.checkLetter();
   }
 
-  constructor(private htmlService: HtmlService, private firstPage: FirstPageComponent) {
+  constructor(private htmlService: HtmlService,
+              private render: Renderer2) {
   }
 
   ngOnInit() {
@@ -88,13 +72,39 @@ export class GameComponent implements OnInit {
     this.timeOfLastLatter = this.start;
     for (let i = 0; i < 8; i++) {
       this.arrayOfGrassIds[i] = i;
-      this.arrayOfPositionByGrass[i] = 0;
     }
-
-    document.getElementById('mountains1').style.left = `${screen.width}`;
-    document.getElementById('treeContainer1').style.left = `${screen.width}`;
-    document.getElementById('grassContainer1').style.left = `${screen.width}`;
-
+    this.backgroundObjects = [
+      {
+        id: this.mountains0,
+        position: -screen.width,
+        distance: 50
+      },
+      {
+        id: this.mountains1,
+        position: screen.width,
+        distance: 50
+      },
+      {
+        id: this.treeContainer0,
+        position: 1,
+        distance: 100
+      },
+      {
+        id: this.treeContainer1,
+        position: screen.width,
+        distance: 100
+      },
+      {
+        id: this.grassContainer0,
+        position: 1,
+        distance: 150
+      },
+      {
+        id: this.grassContainer1,
+        position: screen.width,
+        distance: 150
+      }
+    ];
   }
 
   checkLetter() {
@@ -116,8 +126,6 @@ export class GameComponent implements OnInit {
     }
   }
 
-//  . . . . .()
-
   move() {
     this.checkPosition();
     if (this.finish || this.indexLetter === 0) {
@@ -131,57 +139,17 @@ export class GameComponent implements OnInit {
     requestAnimationFrame(() => this.move());
   }
 
-  moveTree(i: number, position: number) {
-    const tree = document.getElementById('tree' + i);
-    tree.style.transform = `translateX(${ Math.round(position) }px)`;
-    const MAX_POSITION = 200 * (i + 1);
-    if ( position < -MAX_POSITION ) {
-      this.arrayOfPositionByTree[i] = screen.width;
-      tree.style.left = -200 + 'px';
-    } else {
-      const speed = Math.round(this.speedCar / 20);
-      this.arrayOfPositionByTree[i] -= speed;
-    }
-  }
-
-  moveGrass(i: number, position: number) {
-    const grass = document.getElementById('grass' + i);
-    grass.style.transform = `translateX(${ position }px)`;
-    const MAX_POSITION = 300 * (i + 1);
-    if ( position < -MAX_POSITION ) {
-      this.arrayOfPositionByGrass[i] = screen.width;
-      grass.style.left = -120 + 'px';
-    } else {
-      const speed = Math.round(this.speedCar / 10);
-      this.arrayOfPositionByGrass[i] -= speed;
-    }
-  }
-
-  moveMountains() {
-    const mountain = document.getElementById('mountains');
-    mountain.style.transform = `translateX(${ this.positionMountains }px)`;
-    const MAX_POSITION = screen.width;
-    if ( this.positionMountains < -MAX_POSITION ) {
-      this.positionMountains = screen.width;
-      mountain.style.left = -100 + 'px';
-    } else {
-      const speed = Math.round(this.speedCar / 300);
-      this.positionMountains -= speed;
-    }
-  }
-
   checkPosition() {
     const MAX_POSITION = screen.width / 3;
     const speed = this.speedCar; // this may contain const for animation (now remove it)
     this.speedOpponent = Math.random() * 5 + 5;
     if (this.positionCar > MAX_POSITION) {
-      // this.moveMountains();
-      // for (let index = 0; index < this.arrayOfGrassIds.length; index++) {
-      //   this.moveGrass(index, this.arrayOfPositionByGrass[index]);
-      // }
-      // for (let index = 0; index < 7; index++) {
-      //   this.moveTree(index, this.arrayOfPositionByTree[index]);
-      // }
+      this.backgroundObjects.forEach((object) => {
+        object.position -= speed;
+        if (object.position < -screen.width) {
+          object.position += screen.width;
+        }
+      });
       this.positionOpponent += this.speedOpponent - speed;
     } else {
       this.positionCar += speed;
@@ -191,10 +159,11 @@ export class GameComponent implements OnInit {
 
   transformOfBlocks() {
     this.backgroundObjects.forEach((object) => {
-      object.id.style.left = `${object.position}`;
+      console.log(object);
+      this.render.setStyle(object.id.nativeElement, 'left', object.position);
     });
-    document.getElementById('car').style.transform = `translateX(${ Math.round(this.positionCar) }px)`;
-    document.getElementById('carOpponent').style.transform = `translateX(${ Math.round(this.positionOpponent) }px)`;
+    document.getElementById('car').style.left = `(${ Math.round(this.positionCar) }px)`;
+    document.getElementById('carOpponent').style.left = `(${ Math.round(this.positionOpponent) }px)`;
   }
 
 }
