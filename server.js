e289026d8ae911dc0bc7f139b824119a0a67aefd
Vladmaked <makedohska@ukr.net>
2019-05-19 12:01:13 +0300

    add

---
+++ server.js
@@ -0,0 +1,17 @@
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/crazyCarsFront'));
+
+app.get('/*', function(req,res) {
+
+  res.sendFile(path.join(__dirname,'/dist/crazyCarsFront/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
+
